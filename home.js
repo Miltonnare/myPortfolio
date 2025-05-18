e08086a3ce0b42fb62c2fa148d034c1926b991e1
Miltonnare <miltonnareblessed@gmail.com>
2025-05-19 00:55:45 +0300

    Updated the Contact Form

--- home.js
+++ home.js
@@ -8,14 +8,16 @@ const status=document.getElementById("status-message");
 form.addEventListener("submit", function (event){ 
     event.preventDefault();
 
-    const formData=new formData(form)
-    const template={
-        name:formData.get("name"),
-        email:formData.get("email"),
-        subject:formData.get("subject"),
-        message:formData.get("message")
-    };
-    emailjs.send("service_t8chtt6","template_uyc6g6b",template)
+    const formData = {
+                name: form.name.value,
+                email: form.email.value,
+                subject: form.subject.value,
+                message: form.message.value
+            };
+    console.log(formData)
+    
+    
+    emailjs.send("service_t8chtt6","template_uyc6g6b",formData)
     .then(()=>{
         status.textContent="Message has been Sent Succesfully!"
         status.style.color="violet"
